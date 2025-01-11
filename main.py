b87134c9a517e9dfafef5d4c39c8e9bf0cb95280
AnisurRahman06046 <anisurrahman06046@gmail.com>
2025-01-11 23:28:46 +0600

    buttons are listed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout,QVBoxLayout,QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout,QVBoxLayout,QLabel,QGridLayout,QLineEdit
 from PyQt6.QtCore import Qt 
 
 
@@ -9,9 +9,17 @@ main_window.setWindowTitle("Cal2025")
 main_window.resize(600,600)
 
 
+# all objects
 
+display = QLineEdit()
+grid = QGridLayout()
 
-
+buttons = [
+    "7","8","9","/",
+    "4","5","6","*",
+    "1","2","3","-",
+    "0",".","=","+"
+]
 
 
 # run the app
